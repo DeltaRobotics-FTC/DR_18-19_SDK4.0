02829f98860e6a3958402bffdc68bc40e56a4b27
LauraAEvans <elzagirl10@gmail.com>
2018-09-27 20:38:00 -0500

    -Added telemetry for motor lift powers -added a if statement that controls the lift -added lift variable

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumLift.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumLift.java
@@ -7,8 +7,8 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 /**
  * Created by User on 4/19/2018.
  */
-@TeleOp(name="MecanumDrive",group = "")
-public class MecanumDrive extends LinearOpMode
+@TeleOp(name="MecanumLift",group = "")
+public class MecanumLift extends LinearOpMode
 {
     double zScale = 1.0;
     double speed = 1.0;
@@ -18,6 +18,8 @@ public class MecanumDrive extends LinearOpMode
     DcMotor motorLF = null;
     DcMotor motorRB = null;
     DcMotor motorLB = null;
+    DcMotor motorLift = null;
+
     public void runOpMode()
     {
         //Maps motor objects to name in robot configuration
@@ -25,22 +27,21 @@ public class MecanumDrive extends LinearOpMode
         motorRB = hardwareMap.dcMotor.get("motorRB");
         motorLF = hardwareMap.dcMotor.get("motorLF");
         motorLB = hardwareMap.dcMotor.get("motorLB");
+        motorLift = hardwareMap.dcMotor.get("motorLift");
+
+
+
 
         //Setting motors to brake when stopped
         motorRF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLF.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorLB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         motorRB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        motorLift.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
         waitForStart();
         while (opModeIsActive())
         {
-            /*
-            robot.motorRF.setPower(-gamepad1.right_stick_y);
-            robot.motorRB.setPower(-gamepad1.right_stick_y);
-            robot.motorLF.setPower(gamepad1.left_stick_y);
-            robot.motorLB.setPower(gamepad1.left_stick_y);
-            */
 
             //sets motor power according to joystick input
             motorRF.setPower(speed*((-gamepad1.left_stick_y - gamepad1.left_stick_x) - (zScale * gamepad1.right_stick_x)));
@@ -48,11 +49,31 @@ public class MecanumDrive extends LinearOpMode
             motorRB.setPower(speed*(-(-gamepad1.left_stick_x + gamepad1.left_stick_y) - (zScale * gamepad1.right_stick_x)));
             motorLF.setPower(speed*((-gamepad1.left_stick_x + gamepad1.left_stick_y)) + (zScale * gamepad1.right_stick_x));
 
+            //this if statement tell the robot if Y is pressed the arm goes up if A is pressed goes down and if nothings pressed it goes nowhere
+            if (gamepad1.y == true)
+            {
+                motorLift.setPower(100);
+                sleep(500);
+            }
+            else if(gamepad1.a == true)
+            {
+            motorLift.setPower(-100);
+            sleep(500);
+            }
+            else
+            {
+            motorLift.setPower(0);
+            }
+
+
+
+
             //Sends data back to driver station
             telemetry.addData("Motor RF Power", motorRF.getPower());
             telemetry.addData("motor LF power", motorLF.getPower());
             telemetry.addData("Motor RB power", motorRB.getPower());
             telemetry.addData("Motor LF power", motorLF.getPower());
+            telemetry.addData("motor lift power", motorLift.getPower());
 
             telemetry.update();
         }
