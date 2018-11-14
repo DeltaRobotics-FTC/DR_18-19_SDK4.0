4a1802cff1559a3ec36a794dc957ec0b7b24b02b
Delta <deltaroboticsftc@gmail.com>
2018-11-13 20:44:09 -0600

    -Worked on camera testing code -Made camera preview size bigger in layout -Modified bounds we look in for our analysis

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTesting.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CameraTesting.java
@@ -3,11 +3,14 @@ package org.firstinspires.ftc.teamcode;
 import android.graphics.Bitmap;
 import android.graphics.Color;
 
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import for_camera_opmodes.CameraPreview;
 import for_camera_opmodes.LinearOpModeCamera;
 
+@TeleOp (name = "Camera Testing", group = "")
 public class CameraTesting extends LinearOpModeCamera
 {
-    RobotHardware robot = new RobotHardware();
     Drive drive = new Drive();
 
     int mineralColorInt;
@@ -15,93 +18,88 @@ public class CameraTesting extends LinearOpModeCamera
     @Override
     public void runOpMode()
     {
-        robot.init(hardwareMap);
 
-        waitForStart();
-
-        while (opModeIsActive())
-        {
             if(isCameraAvailable())
             {
                 setCameraDownsampling(1);
 
                 startCamera();
 
-                if(imageReady())
-                {
-                    int redValueLeft = -76800;
-                    int blueValueLeft = -76800;
-                    int greenValueLeft = -76800;
+                waitForStart();
 
-                    Bitmap rgbImage;
-                    //The last value must correspond to the downsampling value from above
-                    rgbImage = convertYuvImageToRgb(yuvImage, width, height, 1);
 
-                    telemetry.addData("Width", rgbImage.getWidth());
-                    telemetry.addData("Height", rgbImage.getHeight());
-                    telemetry.update();
+                    if (imageReady()) {
+                        int redValueLeft = -76800;
+                        int blueValueLeft = -76800;
+                        int greenValueLeft = -76800;
 
-                    //This is for only saving the color image if needed.
+                        Bitmap rgbImage;
+                        //The last value must correspond to the downsampling value from above
+                        rgbImage = convertYuvImageToRgb(yuvImage, width, height, 1);
 
-                    for (int x = 480; x < 680; x++)
-                    {
-                        for (int y = 850; y < 1280; y++)
-                        {
-                            if (x == 679 && y >= 850)
-                            {
-                                rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
-                            }
-                            if (x >= 0 && y == 850)
-                            {
-                                rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
-                            }
-                            if (x == 480 && y >= 850)
-                            {
-                                rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
-                            }
-                            if (x >= 480 && y == 1279)
-                            {
-                                rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
+
+                        //This is for only saving the color image if needed.
+
+                        for (int x = 380; x < 580; x++) {
+                            for (int y = 280; y < 480; y++) {
+                                if (x == 579 && y >= 479) {
+                                    rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
+                                }
+                                if (x >= 0 && y == 450) {
+                                    rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
+                                }
+                                if (x == 380 && y >= 450) {
+                                    rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
+                                }
+                                if (x >= 380 && y == 479) {
+                                    rgbImage.setPixel(x, y, Color.rgb(0, 255, 0));
+
+                                }
                             }
                         }
-                    }
 
-                    SaveImage(rgbImage);
 
-                    //Analyzing Jewel Color
-                    for (int x = 550; x < 850; x++) {
-                        for (int y = 900; y < 1280; y++) {
-                            int pixel = rgbImage.getPixel(x, y);
-                            redValueLeft += red(pixel);
-                            blueValueLeft += blue(pixel);
-                            greenValueLeft += green(pixel);
+
+
+                        SaveImage(rgbImage);
+
+                        //Analyzing Jewel Color
+                        for (int x = 550; x < 850; x++) {
+                            for (int y = 900; y < 1280; y++) {
+                                int pixel = rgbImage.getPixel(x, y);
+                                redValueLeft += red(pixel);
+                                blueValueLeft += blue(pixel);
+                                greenValueLeft += green(pixel);
+                            }
                         }
-                    }
-                    redValueLeft = normalizePixels(redValueLeft);
-                    blueValueLeft = normalizePixels(blueValueLeft);
-                    greenValueLeft = normalizePixels(greenValueLeft);
-                    telemetry.addData("redValueLeft", redValueLeft);
-                    telemetry.addData("blueValueLeft", blueValueLeft);
-                    telemetry.addData("greenValueLeft", greenValueLeft);
+                        redValueLeft = normalizePixels(redValueLeft);
+                        blueValueLeft = normalizePixels(blueValueLeft);
+                        greenValueLeft = normalizePixels(greenValueLeft);
+                        /*telemetry.addData("redValueLeft", redValueLeft);
+                        telemetry.addData("blueValueLeft", blueValueLeft);
+                        telemetry.addData("greenValueLeft", greenValueLeft);
+                        telemetry.addData("Width", rgbImage.getWidth());
+                        telemetry.addData("Height", rgbImage.getHeight());
+                        telemetry.update();*/
 
-                   mineralColorInt = highestColor(redValueLeft, blueValueLeft, greenValueLeft);
 
-                    telemetry.addData("Minearl", mineralColorInt);
-                    if (mineralColorInt == 0)
-                    {
-                        telemetry.addData("Mineral Color", "0 : Gold");
-                    } else if (redValueLeft > 100 && blueValueLeft > 100 && greenValueLeft > 100)
-                    {
-                        telemetry.addData("Mineral Color", "White");
-                    }
-                    else {
-                        telemetry.addData("Mineral Color", "Something's Wrong");
-                    }
-                    telemetry.update();
+                        mineralColorInt = highestColor(redValueLeft, blueValueLeft, greenValueLeft);
 
-                    stopCamera();
+                        telemetry.addData("Mineral", mineralColorInt);
+                        if (mineralColorInt == 0) {
+                            telemetry.addData("Mineral Color", "0 : Gold");
+                        } else if (redValueLeft > 100 && blueValueLeft > 100 && greenValueLeft > 100) {
+                            telemetry.addData("Mineral Color", "White");
+                        } else {
+                            telemetry.addData("Mineral Color", "Something's Wrong");
+                        }
+                        telemetry.update();
+                        stopCamera();
+
+
+                    }
                 }
-            }
+
         }
-    }
+
 }
